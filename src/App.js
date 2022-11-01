75319b1054f7fa2985fff1096ccde27b1c90e4aa
Ramiro <ramifazio@gmail.com>
2022-11-01 12:27:08 -0300

    Sugerencias!

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-
-import './App.css';
-import Footer from './components/Footer';
-import ItemListContainer from './components/ItemListContainer';
-import Navbar from './components/Navbar';
-
+import "./App.css";
+import Footer from "./components/Footer";
+import ItemListContainer from "./components/ItemListContainer";
+import Navbar from "./components/Navbar";
 
 export default function App() {
-
   return (
-  <div>
-    <Navbar color={"black"}/>
-    <div>  
-      <ItemListContainer fondo={"whitesmoke"} greeting= {"BIENVENIDOS A CHILL OUT- PRENDAS DE DISEÑO"} />
-     </div>
-    <Footer/>
-  </div>
+    <div>
+      <Navbar color={"black"} />
+      <div>
+        <ItemListContainer fondo={"whitesmoke"} greeting={"BIENVENIDOS A CHILL OUT- PRENDAS DE DISEÑO"} />
+      </div>
+      <Footer />
+    </div>
   );
 }
-
-
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,24 @@
-import MenuIcon from '@mui/icons-material/Menu';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import * as React from 'react';
+import MenuIcon from "@mui/icons-material/Menu";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
 import logo from "../img/logo.png";
-import CartWidget from './CartWidget';
+import CartWidget from "./CartWidget";
 
+export default function Navbar({ color }) {
+  const pages = [
+    //* Mejor poner las pages dentro del componente
+    { label: "Productos", link: "/productos" },
+    { label: "Contacto", link: "/contacto" },
+  ];
 
-const pages = [
-  {label: "Productos", link:"/productos"},
-  {label: "Contacto", link:"/contacto"},
-
-];
-
-export default function Navbar({color}) {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
@@ -39,7 +38,7 @@ export default function Navbar({color}) {
   };
 
   return (
-    <AppBar position="static" className='appbar' style={{backgroundColor: color}} >
+    <AppBar position="static" className="appbar" style={{ backgroundColor: color }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Typography
@@ -49,19 +48,18 @@ export default function Navbar({color}) {
             href="/"
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
-          <img className='logo__navbar' src={logo}  width='90px' height='90px' alt='logonavbar'/>
-      
+            <img className="logo__navbar" src={logo} width="90px" height="90px" alt="logonavbar" />
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -69,34 +67,32 @@ export default function Navbar({color}) {
               aria-haspopup="true"
               onClick={handleOpenNavMenu}
               color="inherit"
-              
             >
-              
               <MenuIcon />
             </IconButton>
             <Menu
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
+                display: { xs: "block", md: "none" },
               }}
             >
               {pages.map((page) => (
                 <MenuItem key={page.label} onClick={handleCloseNavMenu}>
                   <Typography textAlign="center">
-                  <a href=  {page.link}>{page.label}</a>
-                 </Typography>
+                    <a href={page.link}>{page.label}</a>
+                  </Typography>
                 </MenuItem>
               ))}
             </Menu>
@@ -108,36 +104,32 @@ export default function Navbar({color}) {
             href=""
             sx={{
               mr: 2,
-              display: { xs: 'flex', md: 'none' },
+              display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: 'monospace',
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
             CHILL OUT
-          
           </Typography>
-          <Box className='appbar' sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box className="appbar" sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
                 key={page.label}
                 onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block', fontSize: 15 }}
+                sx={{ my: 2, color: "white", display: "block", fontSize: 15 }}
               >
-              <a href= {page.link}> {page.label}</a>
-                
-               
+                <a href={page.link}> {page.label}</a>
               </Button>
             ))}
-          
-         <CartWidget/>
-         </Box>
+
+            <CartWidget />
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>
   );
 }
-
